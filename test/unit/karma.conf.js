fddcd97b649d691b68158e52b2f6b98c17aba815
luizantoniojr <silvajuniorluizantonio@gmail.com>
2018-02-06 14:16:23 -0200

    Remoção da configuração do karma-coverage

--- test/unit/karma.conf.js
+++ test/unit/karma.conf.js
@@ -9,23 +9,18 @@ module.exports = function (config) {
     exclude: [],
     preprocessors: {
       './index.js': ['webpack', 'sourcemap'],
-      'src/**/*.js': ['coverage']
     },
     webpack: webpackConfig,
     webpackMiddleware: {
       noInfo: true
     },
-    reporters: ['progress', 'coverage'],
+    reporters: ['progress'],
     port: 9876,
     colors: true,
     logLevel: config.LOG_INFO,
     autoWatch: true,
     browsers: ['Chrome'],
     singleRun: false,
-    concurrency: Infinity,
-    coverageReporter: {
-      type : 'html',
-      dir : 'coverage/'
-    }
+    concurrency: Infinity
   })
 }
