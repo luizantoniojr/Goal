f4912012234f4dc8e83587b28b77226cd2c21fd2
Luiz Antônio da Silva Júnior <silvajuniorluizantonio@gmail.com>
2018-04-16 21:35:39 -0300

    Total por tipo despesa

--- src/components/Finances/Resume.vue
+++ src/components/Finances/Resume.vue
@@ -15,7 +15,7 @@
               <v-list-tile-title>{{ $t('salary') }}</v-list-tile-title>
             </v-list-tile-content>
               <v-list-tile-content class="align-end">
-                  <v-text-field :placeholder="$t('value')" 
+                  <v-text-field :placeholder="$t('value')" class="salary"
                     v-model="salary" maxlength="10" @blur="salaryOnBlur"
                     :append-icon="salaryInvisible ? 'visibility' : 'visibility_off'"
                     :append-icon-cb="() => (salaryInvisible = !salaryInvisible)"
@@ -32,6 +32,21 @@
             <v-list-tile-content class="align-end">{{ balance }}</v-list-tile-content>
           </v-list-tile>
         </v-list>
+        <v-list three-line>
+        <v-list-tile avatar ripple class="grey lighten-3 expense-item">
+          <v-list-tile-content>
+               <v-select
+                  class="expense-type"
+                  :items="types"
+                  v-model="type"
+                  :label="$t('types_expenses')"
+                  single-line
+                  clearable
+                ></v-select>
+          </v-list-tile-content>
+          <v-list-tile-content class="align-end">{{ totalByExpenseType }}</v-list-tile-content>
+        </v-list-tile>
+       </v-list>
       </v-flex>
     </v-card>
 </template>
@@ -42,7 +57,8 @@ export default {
   components: {},
   data: () => {
     return {
-      salaryInvisible: true
+      salaryInvisible: true,
+      type: null
     };
   },
   computed: {
@@ -65,6 +81,23 @@ export default {
       set(salary) {
         this.$store.commit("setSalary", salary);
       }
+    },
+    types() {
+      return this.$store.getters.typesExpenses;
+    },
+    totalByExpenseType() {
+      if (!this.type) return this.total;
+
+      var total = 0;
+      var expensesByType = this.$store.state.expenses.filter(expense => {
+        return expense.types.some(type => {
+          return type === this.type;
+        });
+      });
+      for (let expense of expensesByType) {
+        total = total + this.$numeral(expense.value).value();
+      }
+      return this.$numeral(total).format("0,0.00");
     }
   },
   mounted() {
@@ -80,7 +113,11 @@ export default {
 </script>
 
 <style scoped>
-.input-group {
+.salary {
   width: 110px;
+}
+
+.expense-type {
+  width: 160px;
 }
 </style>
